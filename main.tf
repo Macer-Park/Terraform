44e80e75b4736102d85a80201ba0d4085340f017
Macer-Park <pkjm1705@gmail.com>
2024-09-18 13:41:50 +0900

    Exercise till 3.5

--- main.tf
+++ main.tf
@@ -1,11 +1,59 @@
-resource "local_file" "abc" {
-  content = "lifecycle - step 3"
-  filename = "${path.module}/abc.txt"
+resource "local_file" "step7" {
+  content = ""
+  filename = "${path.module}/step7.txt"
 
   lifecycle {
-    prevent_destroy = true
+    postcondition {
+      condition = self.content != ""
+      error_message = "content cannot empty"
+    }
   }
 }
+
+# variable "file_name" {
+#   default = "step0.txt"
+# }
+
+# resource "local_file" "step6" {
+#   content = "lifecycle - step 6"
+#   filename = "${path.module}/${var.file_name}"
+
+#   lifecycle {
+#     precondition {
+#       condition = var.file_name == "step6.txt"
+#       error_message = "file name is not \"step6.txt\""
+#     }
+#   }
+# }
+
+# resource "local_file" "abc" {
+#   content = "lifecycle - step 5"
+#   filename = "${path.module}/abc.txt"
+
+#   lifecycle {
+#     ignore_changes = [ 
+#       content
+#      ]
+#   }
+# }
+
+# resource "local_file" "abc" {
+#   content = "lifecycle - step 4"
+#   filename = "${path.module}/abc.txt"
+
+#   lifecycle {
+#     ignore_changes = [  ]
+#   }
+# }
+
+# resource "local_file" "abc" {
+#   content = "lifecycle - step 3"
+#   filename = "${path.module}/abc.txt"
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
 # resource "local_file" "abc" {
 #   content = "lifecycle - step 2"
