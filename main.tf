9a8bb284e0545cbdc27f1fc37bd9f5443fefe733
Macer-Park <pkjm1705@gmail.com>
2024-09-18 13:30:37 +0900

    Exercise till 3.4.4

--- main.tf
+++ main.tf
@@ -1,9 +1,35 @@
 resource "local_file" "abc" {
-  content  = "abc!"
+  content = "lifecycle - step 3"
   filename = "${path.module}/abc.txt"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "local_file" "def" {
-  content = "def!"
-  filename = "${path.module}/def.txt"
-}
+# resource "local_file" "abc" {
+#   content = "lifecycle - step 2"
+#   filename = "${path.module}/abc.txt"
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+# resource "local_file" "abc" {
+#   content = "lifecycle - step 1"
+#   filename = "${path.module}/abc.txt"
+
+#   lifecycle {
+#     create_before_destroy = false
+#   }
+# }
+
+# resource "local_file" "abc" {
+#   content  = "abc!"
+#   filename = "${path.module}/abc.txt"
+# }
+
+# resource "local_file" "def" {
+#   content = "def!"
+#   filename = "${path.module}/def.txt"
+# }
