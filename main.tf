ca9b8bb01caa35cbd5443f422f9b7f030c71f412
Macer-Park <pkjm1705@gmail.com>
2024-09-21 12:57:40 +0900

    Terraform for AWS

--- main.tf
+++ main.tf
@@ -1,83 +1,11 @@
-resource "local_file" "step7" {
-  content = ""
-  filename = "${path.module}/step7.txt"
-
-  lifecycle {
-    postcondition {
-      condition = self.content != ""
-      error_message = "content cannot empty"
-    }
-  }
+provider "aws" {
+  region = "ap-northeast-2"
 }
 
-# variable "file_name" {
-#   default = "step0.txt"
-# }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-# resource "local_file" "step6" {
-#   content = "lifecycle - step 6"
-#   filename = "${path.module}/${var.file_name}"
-
-#   lifecycle {
-#     precondition {
-#       condition = var.file_name == "step6.txt"
-#       error_message = "file name is not \"step6.txt\""
-#     }
-#   }
-# }
-
-# resource "local_file" "abc" {
-#   content = "lifecycle - step 5"
-#   filename = "${path.module}/abc.txt"
-
-#   lifecycle {
-#     ignore_changes = [ 
-#       content
-#      ]
-#   }
-# }
-
-# resource "local_file" "abc" {
-#   content = "lifecycle - step 4"
-#   filename = "${path.module}/abc.txt"
-
-#   lifecycle {
-#     ignore_changes = [  ]
-#   }
-# }
-
-# resource "local_file" "abc" {
-#   content = "lifecycle - step 3"
-#   filename = "${path.module}/abc.txt"
-
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
-
-# resource "local_file" "abc" {
-#   content = "lifecycle - step 2"
-#   filename = "${path.module}/abc.txt"
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-# resource "local_file" "abc" {
-#   content = "lifecycle - step 1"
-#   filename = "${path.module}/abc.txt"
-
-#   lifecycle {
-#     create_before_destroy = false
-#   }
-# }
-
-# resource "local_file" "abc" {
-#   content  = "abc!"
-#   filename = "${path.module}/abc.txt"
-# }
-
-# resource "local_file" "def" {
-#   content = "def!"
-#   filename = "${path.module}/def.txt"
-# }
+  tags = {
+    Name = "terraform-101"
+  }
+}   
