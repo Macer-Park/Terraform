562a340c04d2acf490285327d94b69d0c6bd91b0
Macer-Park <pkjm1705@gmail.com>
2024-09-22 18:29:42 +0900

    Change the exercise style for optimizing directory until finishing the exercise

--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "terraform-101"
+    Name = "macer-park"
   }
 }
 
@@ -17,7 +13,7 @@ resource "aws_subnet" "first_subnet" {
   availability_zone = "ap-northeast-2a"
 
   tags = {
-    Name = "101subnet-1"
+    Name = "public-subnet-1"
   }
 }
 
@@ -28,16 +24,16 @@ resource "aws_subnet" "second_subnet" {
   availability_zone = "ap-northeast-2b"
 
   tags = {
-    Name = "101subnet-2"
+    Name = "public-subnet-2"
   }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+ vpc_id = aws_vpc.main.id
 
-  tags = {
+ tags = {
     Name = "main"
-  }
+ } 
 }
 
 resource "aws_route_table" "route_table" {
@@ -49,38 +45,40 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "route_table_association_1" {
-  subnet_id      = aws_subnet.first_subnet.id
+  subnet_id = aws_subnet.first_subnet.id
   route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_route_table_association" "route_table_association_2" {
-  subnet_id      = aws_subnet.second_subnet.id
+  subnet_id = aws_subnet.second_subnet.id
   route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_subnet" "first_private_subnet" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
 
   availability_zone = "ap-northeast-2a"
 
   tags = {
-    Name = "101subnet-private-1"
-  }
+    Name = "private-subnet-1"
+  }  
 }
+
 resource "aws_subnet" "second_private_subnet" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
   cidr_block = "10.0.4.0/24"
 
   availability_zone = "ap-northeast-2b"
 
   tags = {
-    Name = "101subnet-private-2"
+    Name = "private-subnet-2"
   }
 }
 
 resource "aws_eip" "nat_1" {
   vpc = true
+
   lifecycle {
     create_before_destroy = true
   }
@@ -97,7 +95,6 @@ resource "aws_eip" "nat_2" {
 resource "aws_nat_gateway" "nat_gateway_1" {
   allocation_id = aws_eip.nat_1.id
 
-  # Private subnet이 아닌, Public subnet을 연결해야 함.
   subnet_id = aws_subnet.first_subnet.id
 
   tags = {
@@ -115,7 +112,7 @@ resource "aws_nat_gateway" "nat_gateway_2" {
   }
 }
 
-resource "aws_route_table" "private_route_table_1" {
+resource "aws_route_table" "route_table_private1" {
   vpc_id = aws_vpc.main.id
 
   tags = {
@@ -123,7 +120,7 @@ resource "aws_route_table" "private_route_table_1" {
   }
 }
 
-resource "aws_route_table" "private_route_table_2" {
+resource "aws_route_table" "route_table_private2" {
   vpc_id = aws_vpc.main.id
 
   tags = {
@@ -132,25 +129,24 @@ resource "aws_route_table" "private_route_table_2" {
 }
 
 resource "aws_route_table_association" "route_table_association_private_1" {
-  subnet_id      = aws_subnet.first_private_subnet.id
-  route_table_id = aws_route_table.private_route_table_1.id
+  subnet_id = aws_subnet.first_private_subnet.id
+  route_table_id = aws_route_table.route_table_private1.id
 }
 
 resource "aws_route_table_association" "route_table_association_private_2" {
-  subnet_id      = aws_subnet.second_private_subnet.id
-  route_table_id = aws_route_table.private_route_table_2.id
+  subnet_id = aws_subnet.second_private_subnet.id
+  route_table_id = aws_route_table.route_table_private2.id
 }
 
 resource "aws_route" "private_nat_1" {
-  route_table_id         = aws_route_table.private_route_table_1.id
+  route_table_id = aws_route_table.route_table_private1.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
 }
 
 resource "aws_route" "private_nat_2" {
-  route_table_id         = aws_route_table.private_route_table_2.id
+  route_table_id = aws_route_table.route_table_private2.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway_2.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_2.id 
 }
-
 
